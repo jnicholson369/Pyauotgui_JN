cec0faaa342aaea5e73d133c639fc7a1355390aa
jnicholson369 <33422384+jnicholson369@users.noreply.github.com>
2018-01-08 08:27:56 -0500

    Add files via upload

---
+++ pyautogui_JN.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+pg.moveTo(1091,29,10)
+pg.click(1091,29,1,0)
+pg.typewrite('Memes',.5)
+pg.typewrite(['backspace'],.1)
+pg.typewrite(['backspace'],.1)
+pg.typewrite(['backspace'],.1)
+pg.typewrite(['backspace'],.1)
+pg.typewrite(['backspace'],.1)
+pg.typewrite('Dankest Memes',.5)
+pg.typewrite(['enter'],1)
+pg.moveTo(925,202,7)
+pg.click(925,202,1,0)
